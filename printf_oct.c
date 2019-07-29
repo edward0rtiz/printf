4d739742f2d166c65d9033a62c4d502d720c3149
edward0rtiz <931@holbertonschool.com>
2019-07-29 18:53:19 +0000

    add: funtionc octal using malloc

---
+++ printf_oct.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * printf_oct - prints an octal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int printf_oct(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
+
+	while (num / 8 != 0)
+	{
+		num /= 8;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 8;
+		temp /= 8;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
