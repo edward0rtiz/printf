f54ae3b025e7f2c73944047eaffa3729eb32041c
Jimmer942 <942@holbertonschool.com>
2019-07-29 16:48:32 -0500

    last check

--- _printf.c
+++ _printf.c
@@ -9,7 +9,8 @@ int _printf(const char * const format, ...)
 	convert_match m[] = {
 		{"%s", printf_string},
 		{"%c", printf_char},
-		{"%%", printf_37}
+		{"%%", printf_37},
+		{NULL, NULL}
 	};
 	va_list args;
 	int i = 0, j, len = 0;
@@ -21,7 +22,7 @@ int _printf(const char * const format, ...)
 Here:
 	while (format[i] != '\0')
 	{
-		j = 2;
+		j = 3;
 		while (j >= 0)
 		{
 			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
