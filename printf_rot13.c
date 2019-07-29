63e438132f5961e3935e8f68452aeb1f5daf8208
edward0rtiz <931@holbertonschool.com>
2019-07-29 16:34:29 +0000

    fix: change buffer to putchar

---
+++ printf_rot13.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * printf_rot13 - printf str to ROT13 place into buffer
+ * @args: type struct va_arg where is allocated printf arguments
+ * Return: counter
+ *
+ */
+int printf_rot13(va_list args)
+{
+	int i, j, counter = 0;
+	int k = 0;
+	char *s = va_arg(args, char*);
+	char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char beta[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i]; i++)
+	{
+		k = 0;
+		for (j = 0; alpha[j] && !k; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				_putchar(beta[j]);
+				counter++;
+				k = 1;
+			}
+		}
+		if (!k)
+		{
+			_putchar(s[i]);
+			counter++;
+		}
+	}
+	return (counter);
+}
