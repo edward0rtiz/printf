29f7785c2a50d6e48ea1118bfe8ffa854d25376f
edward0rtiz <931@holbertonschool.com>
2019-07-29 21:54:04 +0000

    add: printf_p

---
+++ printf_p.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+
+/**
+ * printf_hex - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int printf_hex(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+/**
+ * printf_p - prints the address.
+ * @val: arguments.
+ * Return: str.
+ */
+int printf_p(va_list val)
+{
+	unsigned int num;
+	char *str;
+
+	num = (unsigned int)va_arg(val, void *);
+	str = atoi(printf_hex(num);
+	return (str);
+}
