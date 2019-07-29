12ccb632f26c7ed8100e87fa70986913c1e4cea3
edward0rtiz <931@holbertonschool.com>
2019-07-29 15:45:43 +0000

    fix: stirng reverse and rot in putchar no buffer

--- printf_rot13.c
+++ printf_rot13.c
@@ -1,49 +1,37 @@
 #include "holberton.h"
 /**
  * printf_rot13 - printf str to ROT13 place into buffer
- * @stack: type struct va_arg where is allocated printf arguments
- * Return: cp rot13
+ * @args: type struct va_arg where is allocated printf arguments
+ * Return: counter
  *
  */
-
-void printf_rot13(stackvar *stack)
+int printf_rot13(va_list args)
 {
-	int i;
-	int j;
-	int k;
-	char *str, *cp, *alpha, *rot;
+	int i, j, counter = 0;
+	int k = 0;
+	char *s = va_arg(args, char*);
+	char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char beta[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
 
-	alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	rot = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	str = va_arg(*(stack->arguments), char *);
-
-	if (str)
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i]; i++)
 	{
-		k = str;
-	}
-	else
-	{
-		_strlen(str) = 0;
-	}
-	if (1)
-	{
-		cp = calloc(k + 1, sizeof(char));
-
-		for (i = 0; *(str + i) != '\0'; i++)
+		f = 0;
+		for (j = 0; alpha[j] && !k; j++)
 		{
-			for (j = 0; j < 52; j++)
+			if (s[i] == alpha[j])
 			{
-				if (*(str + i) == *(alpha + j))
-				{
-					*(cp + i) = *(rot + j);
-					break;
-				}
+				_putchar(beta[j]);
+				counter++;
+				k = 1;
 			}
-			if (j == 52)
-				*(cp + i) = *(str + j);
 		}
-
-		place_buffer(stack, cp);
-		free(cp);
+		if (!k)
+		{
+			_putchar(s[i]);
+			counter++;
+		}
 	}
+	return (counter);
 }
