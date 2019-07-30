20355da96f2e11e47f95092939f1960a217ba03f
Jimmer942 <942@holbertonschool.com>
2019-07-30 03:12:41 -0500

    fix

--- printf_exclusive_string.c
+++ printf_exclusive_string.c
@@ -13,37 +13,31 @@ int printf_exclusive_string(va_list val)
 
 	s = va_arg(val, char *);
 	if (s == NULL)
-	{
 		s = "(null)";
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
-	else
+	len = _strlen(s);
+	lenaux = lenaux + len;
+	for (i = 0; i < len; i++)
 	{
-		len = _strlen(s);
-		lenaux = lenaux + len;
-		for (i = 0; i < len; i++)
+		if (s[i] < 32 || s[i] >= 127)
 		{
-			if (s[i] < 32 || s[i] >= 127)
+			_putchar('\\');
+			_putchar('x');
+			lenaux = lenaux + 2;
+			cast = s[i];
+			if (cast < 16)
 			{
-				_putchar('\\');
-				_putchar('x');
-				lenaux = lenaux + 2;
-				cast = s[i];
-				if (cast < 16)
-				{
-					_putchar('0');
-					lenaux++;
-				}
-				else
-					lenaux = lenaux + 1;
-				printf_HEX_aux(cast);
-				i++;
+				_putchar('0');
+				lenaux++;
 			}
+			else
+				lenaux = lenaux + 1;
+			printf_HEX_aux(cast);
+			i++;
+		}
+		else
+		{
 			_putchar(s[i]);
 		}
-		return (lenaux);
 	}
+	return (lenaux);
 }
